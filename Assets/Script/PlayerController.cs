af9f63e77872bfb0dbd64b2e7ad863e33473f8d8
Emre <emrekks32@gmail.com>
2021-02-08 23:05:28 +0300

    Camera + Controller

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Settings")]
+    public float PlayerSpeed = 3f;
+    public float PlayerJumpHeight = 1f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+
+    //Gravity
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Grounded();
+    }
+
+    void Movement()
+    {
+        //Movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(direction * PlayerSpeed * Time.deltaTime);
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(PlayerJumpHeight * -2 * gravity);
+        }
+    }
+
+    void Grounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+}

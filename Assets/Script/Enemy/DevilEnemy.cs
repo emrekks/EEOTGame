2f792a1b6ee27c4f068b93ae7eed58fc8c3e90b5
Emir <emirsavas@windowslive.com>
2021-02-09 23:14:45 +0300

    Devil Enemy AI Prototype

---
+++ Assets/Script/Enemy/DevilEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DevilEnemy : MonoBehaviour
+{
+
+    private NavMeshAgent _agent;
+
+    public GameObject player;
+    
+    
+    
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        _agent.SetDestination(player.transform.position);
+    }
+    
+}

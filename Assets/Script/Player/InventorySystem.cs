fa5dafc22a5ecab8c5000612f2d9e5ecb759a1e9
Emre <emrekks32@gmail.com>
2021-02-09 23:13:32 +0300

    InventorSystem Half-Complated

---
+++ Assets/Script/Player/InventorySystem.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySystem : MonoBehaviour
+{
+    
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private int item1;
+    [SerializeField] private int item2;
+    [SerializeField] private float pickupTime = 2f;
+    [SerializeField] private RectTransform pickupImageRoot;
+    [SerializeField] private Image pickupProgressImage;
+    [SerializeField] private TextMeshProUGUI itemNameText;
+
+    private Item itemBeingPickUp;
+    private float currentPickupTimerElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ItemSelect();
+
+        if (HasItemTargetted())
+        {
+            pickupImageRoot.gameObject.SetActive(true);
+
+            if (Input.GetKey(KeyCode.F))
+            {
+                PickupProgressComplete();
+            }
+            else
+            {
+                currentPickupTimerElapsed = 0f;
+            }
+
+            UpdatePickupProgressImage();
+        }
+        else
+        {
+            pickupImageRoot.gameObject.SetActive(false);
+            currentPickupTimerElapsed = 0f;
+        }
+    }
+
+    private bool HasItemTargetted()
+    {
+        return itemBeingPickUp != null;
+    }
+
+    private void PickupProgressComplete()
+    {
+        currentPickupTimerElapsed += Time.deltaTime;
+        if (currentPickupTimerElapsed >= pickupTime)
+        {
+            MoveItemInventory();
+        }
+    }
+    
+    private void UpdatePickupProgressImage()
+    {
+        float pct = currentPickupTimerElapsed / pickupTime;
+        pickupProgressImage.fillAmount = pct;
+    }
+
+    private void ItemSelect()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(Vector3.one / 2f);
+        Debug.DrawRay(ray.origin, ray.direction * 2f, Color.red);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 2f, layerMask))
+        {
+            var hitItem = hitInfo.collider.GetComponent<Item>();
+
+            if (hitItem == null)
+            {
+                itemBeingPickUp = null;
+            }
+
+            else if (hitItem != null && hitItem != itemBeingPickUp)
+            {
+                itemBeingPickUp = hitItem;
+                itemNameText.text = "Pickup" + itemBeingPickUp.gameObject.name;
+            }
+        }
+        else
+        {
+            itemBeingPickUp = null;
+        }
+    }
+
+    private void MoveItemInventory()
+    {
+        itemBeingPickUp.gameObject.SetActive(false);
+        itemBeingPickUp = null;
+    }
+
+}

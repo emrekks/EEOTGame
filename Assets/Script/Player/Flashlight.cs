8e990c88cce3e9c6455a60f9f13cfc49be3f26b6
Emre <emrekks32@gmail.com>
2021-02-09 07:03:03 +0300

    Health System + Flashlight

---
+++ Assets/Script/Player/Flashlight.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public float flashLightDuration = 10f;
+    private Light _light;
+    bool flashlightEnable = false;
+
+    public float timer;
+
+
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+    }
+
+
+    void Update()
+    {
+        FlashlightTurn();
+    }
+
+    void FlashlightTurn()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            flashlightEnable = !flashlightEnable;
+        }
+
+        if (flashlightEnable && flashLightDuration > 0)
+        {
+            _light.enabled = true;
+            flashLightDuration -= Time.deltaTime;
+            timer = 0;
+            if(flashLightDuration <= 0)
+            {
+                flashLightDuration = 0;
+                flashlightEnable = false;
+            }
+
+        }
+
+        if (!flashlightEnable || flashLightDuration <= 0)
+        {
+
+            _light.enabled = false;
+
+            if(timer < 3)
+            {
+                timer += Time.deltaTime;
+            }
+
+            if (flashLightDuration < 3 && timer >= 3)
+            {
+                flashLightDuration += Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Settings")]
+    public float playerSpeed = 3f;
+    public float playerJumpHeight = 1f;
+    public int playerHealth = 100;
+
+    
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    //Dead
+    bool playerDeath = false;
+
+    //Gravity
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+
+    //Descriptions
+    private CharacterController controller;
+    private Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Grounded();
+    }
+
+    void Movement()
+    {
+        
+        if (!playerDeath)
+        {
+            //Movement
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = transform.right * horizontal + transform.forward * vertical;
+            controller.Move(direction * playerSpeed * Time.deltaTime);
+
+            //Jump
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(playerJumpHeight * -2 * gravity);
+            }
+        }
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Grounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    void Health()
+    {
+        if(playerHealth <= 0)
+        {
+            playerDeath = true;
+        }
+
+        if (playerDeath)
+        {
+            //anim.SetBool(death, true);
+        }
+
+    }
+
+}

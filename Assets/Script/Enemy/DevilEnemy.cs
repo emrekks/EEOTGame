c6f024d447f92d9a26f4113893d56dc71c83f3aa
Emir <emirsavas@windowslive.com>
2021-02-10 23:31:15 +0300

    Devil AI Upgrade

--- Assets/Script/Enemy/DevilEnemy.cs
+++ Assets/Script/Enemy/DevilEnemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,12 +7,16 @@ using UnityEngine.AI;
 public class DevilEnemy : MonoBehaviour
 {
 
-    private NavMeshAgent _agent;
+    private NavMeshAgent _agent; 
+    [SerializeField]private float[] distance;
+    [SerializeField]private GameObject _target;
 
-    public GameObject player;
-    
-    
-    
+    public float range;
+
+    [SerializeField]private bool playerSelected;
+
+
+
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
@@ -20,7 +25,45 @@ public class DevilEnemy : MonoBehaviour
     
     void Update()
     {
-        _agent.SetDestination(player.transform.position);
+        NearestPlayer();
+
+        if (playerSelected)
+        {
+            _agent.SetDestination(_target.transform.position);
+        }
+        
     }
-    
+
+    void NearestPlayer()
+    {
+        float minDistance = range;
+        var players= GameObject.FindGameObjectsWithTag("Player");
+        
+        foreach (var player in players)
+        {
+            for (int i = 0; i < players.Length; i++)
+            {
+                float thisDistance = Mathf.Abs(Vector3.Distance(players[i].transform.position, transform.position));
+
+                distance[i] = thisDistance;
+
+                if (thisDistance < minDistance)
+                {
+                    minDistance = thisDistance;
+                    _target = players[i];
+                    playerSelected = true;
+                }
+                
+            }
+            
+        }
+        
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position,range);
+    }
 }
